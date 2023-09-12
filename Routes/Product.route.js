b32715cb47c06bbda68486f29e8f4e038ad1369f
Y <techprivacy@gmail.com>
2023-09-12 20:47:25 +0530

    test

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const prouter = express.Router();
 const Product = require("../Models/Product.model");
 
+//Post A Product Data
 prouter.post("/", async (req, res) => {
   try {
     const product = new Product(req.body);
@@ -12,6 +13,7 @@ prouter.post("/", async (req, res) => {
   }
 });
 
+//Get All product Data
 //Two Optional Parameters 1.Query 2.Projection
 prouter.get("/", async (req, res) => {
   try {
@@ -22,15 +24,40 @@ prouter.get("/", async (req, res) => {
   }
 });
 
-prouter.get("/:name", async (req, res) => {
-  const pname = req.params.name;
+//Get single product Data
+prouter.get("/:id", async (req, res) => {
+  const id = req.params.id;
   try {
-    const prodcut = await Product.findOne({ name: pname });
+    const prodcut = await Product.findById(id);
     res.send(prodcut);
   } catch (error) {
     console.log("Error");
   }
 });
 
+// Delete a Product
+prouter.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const prodcut = await Product.findByIdAndDelete(id);
+    res.send(prodcut);
+    console.log("Product Deleted");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+prouter.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const updates = req.body;
+    const options = { new: true };
+    const result = await Product.findByIdAndUpdate(id, updates, options);
+    res.send(result);
+    console.log("Product Updated");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = prouter;
